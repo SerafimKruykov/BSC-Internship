2830bfcfc4b72ab1b0c0585552e95f5553b6c305
SerafimKruykov <serafim.kruykov@gmail.com>
2021-12-04 16:26:12 +0300

    Task 2

--- app/src/main/java/com/example/note/MainActivityPresenter.kt
+++ app/src/main/java/com/example/note/MainActivityPresenter.kt
@@ -2,15 +2,15 @@ package com.example.note
 
 /**
  * Класс слушает события во view и обрабатывает их
- * @param view - ссылка на активити, которая реализует интерфейс NoteView
+ * @param view ссылка на активити, которая реализует интерфейс NoteView
  */
 
 class MainActivityPresenter(private val view: NoteView){
 
     /**
      * Обработка нажатия на кнопку "Сохранить"
-     * @param header - название заметки
-     * @param content - текст заметки
+     * @param header название заметки
+     * @param content текст заметки
      */
     fun tryToSave(header: String, content: String){
         if(header.isEmpty() && content.isEmpty()) view.onEmptyNote() else view.onSaved()
@@ -25,8 +25,8 @@ class MainActivityPresenter(private val view: NoteView){
 
     /**
      * Обработка нажатия на кнопку "Поделиться"
-     * @param header - название заметки
-     * @param content - текст заметки
+     * @param header название заметки
+     * @param content текст заметки
      */
     fun tryToShare(header: String, content: String){
         if(header.isEmpty() && content.isEmpty()) view.onEmptyNote() else view.shareNote(header, content)
